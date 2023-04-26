18a756156dcdbe19272f3c86b4da11d8d1bc2b37
Maria <marykrnv@bk.ru>
2023-04-26 16:48:40 +0300

    change feature to step

--- pages/ya_search_page.py
+++ pages/ya_search_page.py
@@ -1,15 +1,11 @@
 import allure
 
-from pages.base_page import BasePage
 from locators.ya_page_locators import LocatorYaPage
+from pages.base_page import BasePage
 
 
 class YaSearchPage(BasePage):
 
-    @allure.feature('Получить элемент Сейчас в СМИ')
+    @allure.step('Получить элемент Сейчас в СМИ')
     def get_element_nav_bar(self):
         return self.find_element(LocatorYaPage.LOCATOR_NAV_BAR)
-
-    @allure.feature('Закрыть модальное окно')
-    def click_on_x(self):
-        return self.click_on_button(LocatorYaPage.LOCATOR_X)
