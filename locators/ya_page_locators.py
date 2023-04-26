1978f8667cebe1d60cad4d53d03ebabb3bcf5164
Maria <marykrnv@bk.ru>
2023-04-26 16:50:25 +0300

    del statics, add assert messages

--- locators/ya_page_locators.py
+++ locators/ya_page_locators.py
@@ -2,5 +2,4 @@ from selenium.webdriver.common.by import By
 
 
 class LocatorYaPage:
-    LOCATOR_NAV_BAR = (By.XPATH, ".//div[@aria-label='Новости на тему']")  # "Логотип Дзен"
-    LOCATOR_X = (By.XPATH, ".//span[@class='b69e8b88c ea96a8268 effa5c4d0 be4aa8ece']")
+    LOCATOR_NAV_BAR = (By.XPATH, ".//div[text()='Сейчас в СМИ']")  # "Сейчас в СМИ"
--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -1,5 +1,5 @@
-import pytest
 import allure
+import pytest
 
 from pages.order_page import OrderPage
 from pages.start_page import StartPage
@@ -8,20 +8,18 @@ from pages.ya_search_page import YaSearchPage
 
 class TestOrderPage:
 
-    @staticmethod
     @allure.title('Нажать кнопку «Заказать» в хедере')
-    def test_go_to_order_page_on_header_order_button(driver):
+    def test_go_to_order_page_on_header_order_button(self, driver):
         start_page = StartPage(driver)
         start_page.go_to_web()
         start_page.click_on_header_order_button()
         order_page = OrderPage(driver)
         header = order_page.check_order_header()
 
-        assert header.text == 'Для кого самокат'
+        assert header.text == 'Для кого самокат', f'Фактический результат: {header.text}'
 
-    @staticmethod
     @allure.title('Нажать кнопку «Заказать» в блоке "Как это работает"')
-    def test_go_to_order_page_on_how_it_work_order_button(driver):
+    def test_go_to_order_page_on_how_it_work_order_button(self, driver):
         start_page = StartPage(driver)
         start_page.go_to_web()
         start_page.scroll_to_how_it_works()
@@ -29,21 +27,20 @@ class TestOrderPage:
         order_page = OrderPage(driver)
         header = order_page.check_order_header()
 
-        assert header.text == 'Для кого самокат'
+        assert header.text == 'Для кого самокат', f'Фактический результат: {header.text}'
 
-    @staticmethod
-    @allure.title('Заполнить форму заказа. Проверить, что появилось всплывающее окно с сообщением об успешном создании заказа')
+    @allure.title('Заполнить форму заказа. Проверить, что появилось всплывающее окно с сообщением об успешном создании'
+                  ' заказа')
     @pytest.mark.parametrize(
-        argnames=['name', 'lastname', 'address', 'metro', 'phone'],
-        argvalues=[('Ольга', 'Ким', 'ул. Митинская, д. 9', 'Митино', '99999999999'),
-                   ('Сергей', 'Фокс', 'ул. Фрунзе, д. 9', 'Фрунзенская', '99997777777'),
+        argnames=['name', 'lastname', 'address', 'metro', 'phone', 'date', 'period', 'color', 'comment',
+                  'expected_result'],
+        argvalues=[('Ольга', 'Ким', 'ул. Митинская, д. 9', 'Митино', '99999999999', '27.10.2023', 'сутки',
+                    'черный жемчуг', 'Тест', 'Заказ оформлен'),
+                   ('Сергей', 'Фокс', 'ул. Фрунзе, д. 9', 'Фрунзенская', '99997777777', '17.4.2023', 'двое суток',
+                    'серая безысходность', 'Тестовый тест', 'Заказ оформлен'),
                    ])
-    @pytest.mark.parametrize(
-        argnames=['date', 'period', 'color', 'comment'],
-        argvalues=[('27.10.2023', 'сутки', 'черный жемчуг', 'Тест'),
-                   ('17.4.2023', 'двое суток', 'серая безысходность', 'Тестовый тест'),
-                   ])
-    def test_create_new_order(driver, name, lastname, address, metro, phone, date, period, color, comment):
+    def test_create_new_order(self, driver, name, lastname, address, metro, phone, date, period, color, comment,
+                              expected_result):
         order_page = OrderPage(driver)
         order_page.go_to_web('order')
         order_page.click_on_cookie_button()
@@ -52,27 +49,25 @@ class TestOrderPage:
         order_page.click_on_agree_button()
         header = order_page.check_modal_header()
 
-        assert 'Заказ оформлен' in header.text
+        assert expected_result in header.text, \
+            f'Ожидаемый результат: {expected_result}, Полученный результат: {header.text}'
 
-    @staticmethod
     @allure.title('Проверить: если нажать на логотип «Самоката», попадёшь на главную страницу «Самоката».')
-    def test_go_to_main_page_from_order_page(driver):
+    def test_go_to_main_page_from_order_page(self, driver):
         order_page = OrderPage(driver)
         order_page.go_to_web('order')
         order_page.click_on_samokat()
         start_page = StartPage(driver)
         header = start_page.check_main_header()
 
-        assert 'Самокат' in header.text
+        assert 'Самокат' in header.text, f'Фактический результат: {header.text}'
 
-    @staticmethod
     @allure.title('Проверить: если нажать на логотип Яндекса, в новом окне откроется главная страница Яндекса.')
-    def test_go_to_ya_page_from_order_page(driver):
+    def test_go_to_ya_page_from_order_page(self, driver):
         order_page = OrderPage(driver)
         order_page.go_to_web('order')
         order_page.click_on_ya()
         ya_page = YaSearchPage(driver)
-        ya_page.click_on_x()
         element = ya_page.get_element_nav_bar()
 
-        assert 'Сейчас в СМИ' in element
+        assert 'Сейчас в СМИ' in element.text, f'Фактический результат: {element.text}'
